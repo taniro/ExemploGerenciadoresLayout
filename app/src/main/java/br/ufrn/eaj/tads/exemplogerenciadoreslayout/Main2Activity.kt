687367b5da8937748582d6957a0152f7cf079664
taniro <tanirocr@gmail.com>
2021-02-09 09:37:11 -0300

    update 2021

--- app/src/main/java/br/ufrn/eaj/tads/exemplogerenciadoreslayout/Main2Activity.kt
+++ app/src/main/java/br/ufrn/eaj/tads/exemplogerenciadoreslayout/Main2Activity.kt
@@ -3,13 +3,21 @@ package br.ufrn.eaj.tads.exemplogerenciadoreslayout
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main2.*
+import android.widget.Button
+import android.widget.ProgressBar
 
 class Main2Activity : AppCompatActivity() {
+
+    lateinit var progressBar:ProgressBar
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
+        progressBar = findViewById(R.id.progressbar)
+        button = findViewById(R.id.button)
 
         button.setOnClickListener {
             progressBar.visibility = View.VISIBLE
--- app/src/main/java/br/ufrn/eaj/tads/exemplogerenciadoreslayout/Main7Activity.kt
+++ app/src/main/java/br/ufrn/eaj/tads/exemplogerenciadoreslayout/Main7Activity.kt
@@ -5,29 +5,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main7.*
 
 class Main7Activity : AppCompatActivity() {
+
+    lateinit var linearLayout: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main7)
+
+        linearLayout = findViewById(R.id.principal)
 
         /*
         for (i in 1..100){
             var text = TextView(this)
             text.setLayoutParams(LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT))
             text.text = "Texto $i"
-            principal.addView(text)
+            linearLayout.addView(text)
         }
-        */
 
+         */
         for (i in 1..100){
             val inflater = LayoutInflater.from(this)
-            val tv = inflater.inflate(R.layout.infla_texto, principal, false) as TextView
+            val tv = inflater.inflate(R.layout.infla_texto, linearLayout, false) as TextView
             tv.text = "Texto com Inflater $i"
-            principal.addView(tv)
+            linearLayout.addView(tv)
         }
+
 
 
     }
